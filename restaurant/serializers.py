cac38daab279ebb357b153d39b3e5e95361f5867
Samvit Jain <jain.samvit@gmail.com>
2024-08-07 05:08:35 +0530

    Added booking api

---
+++ restaurant/serializers.py
@@ -0,0 +1,13 @@
+from rest_framework import serializers
+from .models import Booking, Menu
+
+
+class MenuSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Menu
+        fields = ['id', 'title', 'price', 'inventory']
+
+class BookingSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Booking
+        fields = ['id', 'name', 'no_of_guests', 'booking_date']
